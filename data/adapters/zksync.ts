8d40d5dde5710b22b76137bda9312d563b1b2075
David Mihal <david@dmihal.com>
2021-09-06 15:57:11 +0100

    Fix ZKSync fee

--- data/adapters/zksync.ts
+++ data/adapters/zksync.ts
@@ -3,14 +3,14 @@ import { Context } from '@cryptostats/sdk';
 export function setup(sdk: Context) {
   const getFeeForTransfer = async () => {
     const feeData = await sdk.http.post('https://api.zksync.io/jsrpc', {
-      'jsonrpc': '2.0',
+      jsonrpc: '2.0',
       id: 1,
       method: 'get_tx_fee',
-      params: ["Transfer", "0xC8568F373484Cd51FDc1FE3675E46D8C0dc7D246", "DAI"],
+      params: ['Transfer', '0x4353b45f148b9a095c015a8afe42b46af2c94757', 'DAI'],
     });
 
     return feeData.result.totalFee / 1e18;
-  }
+  };
 
   sdk.register({
     id: 'zksync-v1',
@@ -20,7 +20,10 @@ export function setup(sdk: Context) {
       // feeSwap: getFeeResolverForCost(2602626),
     },
     metadata: {
-      icon: sdk.ipfs.getDataURILoader('QmXeCkZTkG8nuNAMbNykxfu1ybJHyDsu8EgVJ7RKuza5WA', 'image/svg+xml'),
+      icon: sdk.ipfs.getDataURILoader(
+        'QmXeCkZTkG8nuNAMbNykxfu1ybJHyDsu8EgVJ7RKuza5WA',
+        'image/svg+xml'
+      ),
       category: 'l2',
       name: 'ZKSync',
       description: 'ZKSync is a ZK Rollup that supports transfers of any token',
