77e578014f44fa414a5051431d53c591e91f7344
David Mihal <david@dmihal.com>
2021-12-13 23:32:57 +0000

    Move Arbitrum to cryptostats

--- data/adapters/index.ts
+++ data/adapters/index.ts
@@ -1,13 +1,11 @@
 import sdk from '../sdk';
 
 import { setup as setupEthereum } from './ethereum';
-import { setup as setupArbitrum } from './arbitrum';
 import { setup as setupAztec } from './aztec';
 import { setup as setupLoopring } from './loopring';
 
 const feesList = sdk.getList('l2-fees');
 
 feesList.addAdaptersWithSetupFunction(setupEthereum);
-feesList.addAdaptersWithSetupFunction(setupArbitrum);
 feesList.addAdaptersWithSetupFunction(setupAztec);
 feesList.addAdaptersWithSetupFunction(setupLoopring);
