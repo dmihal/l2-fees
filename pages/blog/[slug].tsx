c443fde9c3d616e1c5d0c98d172bcd5629c6caf0
David Mihal <david@dmihal.com>
2022-08-25 17:23:52 +0100

    Style blog page

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -12,6 +12,10 @@ interface BlogProps {
 }
 
 const components = {
+  h1: () => null,
+  h2: ({ children }: { children: string }) => (
+    <h2 id={children.toLowerCase().replaceAll(' ', '-')}>{children}</h2>
+  ),
   Tweet,
   LightXfer: dynamic(() => import('components/blog-widgets/LightXfer')),
 };
@@ -27,56 +31,96 @@ export const Blog: NextPage<BlogProps> = ({ post }) => {
       />
 
       <div className="subtitle">L2Fees Blog</div>
-      <div>
+      <div className="nav-links">
         <Link href="/blog">
-          <a>Back</a>
+          <a>Posts</a>
+        </Link>
+        {' | '}
+        <Link href="/">
+          <a>Home</a>
         </Link>
       </div>
-      <h1 className="title">{post.title}</h1>
 
-      <MDXRemote components={components as any} {...post.contentSource} />
+      <h1 className="title">{post.title}</h1>
+      <div className="date">
+        {new Date(post.date).toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}
+      </div>
+
+      {post.metadata.author && (
+        <div>
+          {post.metadata.authorLink ? (
+            <a href={post.metadata.authorLink}>{post.metadata.author}</a>
+          ) : (
+            post.metadata.author
+          )}
+        </div>
+      )}
+
+      <article>
+        <MDXRemote components={components as any} {...post.contentSource} />
+      </article>
 
       <style jsx>{`
         main {
           padding: 2rem 0 3rem;
           flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
           width: 100%;
+          max-width: 650px;
         }
 
         .title {
-          margin: 0 0 16px;
+          margin: 10px 0 16px;
           line-height: 1.15;
-          font-size: 4rem;
+          font-size: 32px;
           font-weight: 700;
         }
 
-        .title,
-        .description {
-          text-align: center;
-          max-width: 800px;
+        .nav-links,
+        .nav-links a {
+          font-size: 14px;
+          color: #555;
+        }
+        .nav-links a:hover {
+          color: #999;
         }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-          margin: 4px 0 20px;
+        .subtitle {
+          text-transform: uppercase;
+          font-weight: bold;
+          font-size: 20px;
+          letter-spacing: -1px;
+          color: #333;
         }
 
-        .heart {
-          margin: 0 0 18px 0;
-          font-size: 18px;
-          font-style: italic;
+        .date {
+          color: #333;
         }
 
-        .toolbar {
-          max-width: 600px;
-          width: 100%;
-          display: flex;
-          justify-content: flex-end;
+        article {
+          margin: 24px 0;
+        }
+        article :global(h2) {
+          margin: 16px 0 4px;
+          font-size: 26px;
+        }
+        article :global(h3) {
+          margin: 16px 0 4px;
+          font-size: 22px;
+        }
+        article :global(p) {
+          margin: 0 0 22px;
+          font-size: 20px;
+          line-height: 150%;
+        }
+        article :global(li) {
+          font-size: 20px;
+        }
+        article :global(hr) {
+          margin: 32px 0;
         }
       `}</style>
     </main>
